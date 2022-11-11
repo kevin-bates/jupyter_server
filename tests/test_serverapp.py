209cd3e63a5e7b281370126a0df8ea29626f6474
Kevin Bates <kbates4@gmail.com>
2022-11-11 15:50:14 -0800

    Fix linting

--- tests/test_serverapp.py
+++ tests/test_serverapp.py
@@ -6,7 +6,6 @@ from unittest.mock import patch
 
 import pytest
 from jupyter_core.application import NoStart
-from tornado.web import HTTPError
 from traitlets import TraitError
 from traitlets.tests.utils import check_help_all_output
 
